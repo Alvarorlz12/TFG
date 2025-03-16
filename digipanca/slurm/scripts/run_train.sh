ea2733e75eab148c8b175c5cf77d304ec4737701
Alvaro R <alvaroruiz27@correo.ugr.es>
2025-03-16 20:57:26 +0100

    Update run_train.sh
    Updated for using up to 2 params instead of only one: EXPERIMENT_NAME CONFIG_NAME, which allows the execution of experiments with different config name. If the CONFIG_NAME is not provided, then the EXPERIMENT_NAME is used.

--- digipanca/slurm/scripts/run_train.sh
+++ digipanca/slurm/scripts/run_train.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
+# Este script se encarga de lanzar un experimento de entrenamiento en el cluster
+# de la UGR. Para ello, se le debe pasar el nombre del experimento y, opcionalmente,
+# el nombre de la configuración a utilizar. Si no se especifica, se utilizará la
+# configuración con nombre igual al del experimento.
 if [ -z "$1" ]; then
-    echo "Uso: $0 <nombre_experimento>"
+    echo "Uso: $0 <nombre_experimento> [<nombre_configuración>]"
     exit 1
 fi
 
 EXPERIMENT_NAME=$1
+CONFIG_NAME=${2:-$1}    # Si no se especifica, se utiliza el nombre del experimento
 
 # Pasar el script modificado directamente a sbatch a través de stdin
-sed "s/EXPERIMENT_NAME/${EXPERIMENT_NAME}/g" slurm/scripts/train_template.sh | sbatch
+sed -e "s/EXPERIMENT_NAME/${EXPERIMENT_NAME}/g" \
+    -e "s/CONFIG_NAME/${CONFIG_NAME}/g" \
+    slurm/scripts/train_template.sh | sbatch
